9a16ca4d5f34a2b919cc73ed204c7bba76d2bae2
nvardhanveerati <61690078+nvardhanveerati@users.noreply.github.com>
2022-06-19 14:18:37 -0700

    Update app_tier code

--- app_tier/app_module.py
+++ app_tier/app_module.py
@@ -16,11 +16,18 @@ import json
 import sys
 import time
 
-def encode_image(image_path):
 
+input_queue_url = "https://sqs.us-east-1.amazonaws.com/013922704123/RequestQueue"
+output_queue_url = "https://sqs.us-east-1.amazonaws.com/013922704123/ResponseQueue"
+INPUT_BUCKET = "new-input-bucket"
+OUTPUT_BUCKET = "output-imagedataset-bucket"
+
+
+def encode_image(image_path):
     with open(image_path, "rb") as img_file:
         b64_string = base64.b64encode(img_file.read())
     return b64_string
+
 
 def decode_image_and_write(file_path, image_string):
     with open(file_path, 'wb') as fh:
@@ -28,7 +35,6 @@ def decode_image_and_write(file_path, image_string):
 
 
 def classify_image(image_path):
-
     url = str(image_path)
     #img = Image.open(urlopen(url))
     img = Image.open(url)
@@ -50,11 +56,6 @@ def classify_image(image_path):
 
     return result
 
-
-input_queue_url = "https://sqs.us-east-1.amazonaws.com/013922704123/RequestQueue"
-output_queue_url = "https://sqs.us-east-1.amazonaws.com/013922704123/ResponseQueue"
-INPUT_BUCKET = "new-input-bucket"
-OUTPUT_BUCKET = "output-imagedataset-bucket"
 
 def send_message(queue_url, sqs_client, name, body):
     print("Sending message to SQS queue")
@@ -84,14 +85,22 @@ def recieve_sqs_message(sqs_client):
         print(e)
         return None
 
+
 def number_messages_in_queue(sqs_client, queue_url):
 	att = sqs_client.get_queue_attributes(QueueUrl = queue_url, AttributeNames = ['All'])
 	x = int(att['Attributes']['ApproximateNumberOfMessages'])
 	print(str(x)+" MESSAGES PRESENT")
 	return x
 
+
 def delete_message(sqs_client, queue_url, receipt_handle):
-	sqs_client.delete_message(QueueUrl = queue_url, ReceiptHandle = receipt_handle)
+	try:
+		sqs_client.delete_message(QueueUrl = queue_url, ReceiptHandle = receipt_handle)
+	except Exception as e:
+		print("Unable to delete message")
+		print(e)
+		print()
+
 
 if __name__ == '__main__':
 	try:
@@ -112,40 +121,29 @@ if __name__ == '__main__':
 				break
 			response = recieve_sqs_message(sqs_client = sqs_obj)
 		rec_handle = response["Messages"][0]["ReceiptHandle"]
-
 		loaded_response = json.loads(response["Messages"][0]["Body"])
 
 		encoded_img = bytes(loaded_response["encoded_image"], 'utf-8')
 		image_name = loaded_response["file_name"]
-
 		print("Recieved the Image: ", image_name)
-
 		image_path = image_name
-
 		decode_image_and_write(image_path, encoded_img)
-
 		classified_label = classify_image(image_path)
-
 		s3_client = boto3.resource('s3', region_name='us-east-1')
 
 		s3_client.Bucket(INPUT_BUCKET).upload_file(image_path, image_name)
-		print('Input image file uploaded to s3!')
+		print('Image uploaded to S3')
 
 		s3_client.Object(OUTPUT_BUCKET, str( image_name.split(".")[0] ) ).put(Body=classified_label)
-		print('Output file uploaded to s3!')
+		print('Classification txt file uploaded to s3')
 
 		sqs_message_body = {
                 'classification': classified_label,
                 'file_name': image_name
         }
 
-		sqs_obj.send_message(
-			QueueUrl=output_queue_url,
-			MessageBody=json.dumps(sqs_message_body)
-		)
-		print("SQS Results Queue uploaded")
-
-
+		sqs_obj.send_message(QueueUrl=output_queue_url, MessageBody=json.dumps(sqs_message_body))
+		print("Classification result sent to response queue")
 
 		delete_message(sqs_client = sqs_obj, queue_url = input_queue_url, receipt_handle = rec_handle)
 		# print("sleeping for 15 secs")
