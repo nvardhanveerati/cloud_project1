e27bc14857bd49e432694535a4246ef51b6509ff
KshitijSrivastava <kshitij127@yahoo.co.in>
2022-06-26 14:49:10 -0700

    Made the app faster using hashmap

--- web_tier/app.py
+++ web_tier/app.py
@@ -6,6 +6,8 @@ import base64
 import json
 import os
 
+hs = {}
+
 app = Flask(__name__)
 app.secret_key = "something unique"
 
@@ -32,14 +34,26 @@ def process():
             print("Message sent to request queue")
 
         while True:
+            if Img_file.filename in hs:
+                sqs.delete_message(QueueUrl=SQS_response, ReceiptHandle=hs[Img_file.filename][1] )
+                classification = hs[Img_file.filename][0]
+                del hs[ Img_file.filename ]
+                return classification
+
             received_mess_1 = sqs.receive_message(QueueUrl=SQS_response, AttributeNames=['All'], MaxNumberOfMessages=10, WaitTimeSeconds=1)
             received_mess_list = received_mess_1.get('Messages', [])
             for r_mess in received_mess_list:
                 body = r_mess.get('Body')
                 contnt = json.loads(body)
-                if(Img_file.filename==contnt['file_name']):  
-                    sqs.delete_message(QueueUrl=SQS_response, ReceiptHandle=r_mess.get('ReceiptHandle'))
-                    return contnt.get('classification')
+                #if(Img_file.filename==contnt['file_name']):
+
+                hs[contnt['file_name']] = [ contnt.get('classification') , r_mess.get('ReceiptHandle') ]
+
+            if Img_file.filename in hs:
+                sqs.delete_message(QueueUrl=SQS_response, ReceiptHandle= hs[Img_file.filename][1]  )
+                classification = hs[Img_file.filename][0]
+                del hs[ Img_file.filename ] 
+                return classification
                     #return Img_file.filename
     except Exception as e:
         print("Couldn't Process the images")
